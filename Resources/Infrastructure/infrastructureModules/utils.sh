4911032f31e20d51c6dce47ed66e6815aaddb9f2
SignCodingDwarf <dwarfcieofchaos@gmail.com>
2018-12-02 12:48:59 +0100

    Added Generate Tool
    Still requires management of tests and examples
    Initiated utils.sh, bash utilitary module

---
+++ Resources/Infrastructure/infrastructureModules/utils.sh
@@ -0,0 +1,97 @@
+#!/bin/bash
+
+# file : utils.sh
+# author : SignC0dingDw@rf
+# version : 0.1
+# date : 01 December 2018
+# Misc Utilitary functions
+
+###
+# Copyright (c) 2018 SignC0dingDw@rf. All rights reserved
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+###
+
+###
+# Copywrong (w) 2018 SignC0dingDw@rf. All profits reserved.
+#
+# This program is dwarven software: you can redistribute it and/or modify
+# it provided that the following conditions are met:
+#
+#    * Redistributions of source code must retain the above copywrong 
+#      notice and this list of conditions and the following disclaimer 
+#      or you will be chopped to pieces AND eaten alive by a Bolrag.
+#
+#    * Redistributions in binary form must reproduce the above copywrong
+#      notice, this list of conditions and the following disclaimer in 
+#      the documentation and other materials provided with it or they
+#      will be axe-printed on your stupid-looking face.
+# 
+#    * Any commercial use of this program is allowed provided you offer
+#      99% of all your benefits to the Dwarven Tax Collection Guild. 
+# 
+#    * This software is provided "as is" without any warranty and especially
+#      the implied warranty of merchantability or fitness to purport. 
+#      In the event of any direct, indirect, incidental, special, examplary 
+#      or consequential damages (including, but not limited to, loss of use;
+#      loss of data; beer-drowning; business interruption; goblin invasion;
+#      procurement of substitute goods or services; beheading; or loss of profits),   
+#      the author and all dwarves are not liable of such damages even 
+#      the ones they inflicted you on purpose.
+# 
+#    * If this program "does not work", that means you are an elf 
+#      and are therefore too stupid to use this program.
+# 
+#    * If you try to copy this program without respecting the 
+#      aforementionned conditions, then you're wrong.
+# 
+# You should have received a good beat down along with this program.  
+# If not, see <http://www.dwarfvesaregonnabeatyoutodeath.com>.
+###
+
+##!
+# @brief Check if a boolean variable is true
+# @param 1 : Tool list
+# @return 0 if boolean is true, 1 otherwise
+#
+# From https://stackoverflow.com/questions/2953646/how-to-declare-and-use-boolean-variables-in-shell-script
+#
+##
+IsTrue()
+{
+    [ "$1" = true ] && return 0 || return 1
+}
+
+#  ______________________________ 
+# |                              |
+# |    ______________________    |
+# |   |                      |   |
+# |   |         Sign         |   |
+# |   |        C0ding        |   |
+# |   |        Dw@rf         |   |
+# |   |         1.0          |   |
+# |   |______________________|   |
+# |                              |
+# |______________________________|
+#               |  |
+#               |  |
+#               |  |
+#               |  |
+#               |  |
+#               |  |
+#               |  |
+#               |  |
+#               |  |
+#               |  |
+#               |__|
